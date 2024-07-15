40220fa859c37a41030677b09ac745f4cbe5de4f
farialimpa <farialimpa57525@gmail.com>
2024-07-15 23:12:17 +0600

    navber

---
+++ script.js
@@ -0,0 +1,6 @@
+const navDialog = document.getElementById('nav-dialog');
+function handleMenu() {
+    navDialog.classList.toggle('hidden');
+    console.log(navDialog.classList, "class list");
+    console.log(document);
+}
